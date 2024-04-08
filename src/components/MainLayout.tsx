579c80f2291636aa54b127633cb4a26ca4d4e074
Kevin Misigaro <kevin@databalk.nu>
2024-04-08 15:58:03 +0200

    ci: fixing deployment of Navbar

--- src/components/MainLayout.tsx
+++ src/components/MainLayout.tsx
@@ -1,15 +1,13 @@
-import { ReactNode } from 'react'
-import Navbar from './Navbar'
+import { ReactNode } from "react";
+import Navbar from "./Navbar";
 
-function MainLayout({children}: {children : ReactNode}) {
+function MainLayout({ children }: { children: ReactNode }) {
   return (
     <div>
-        <Navbar/>
-       <div className='px-20 pt-8'>
-       {children}
-       </div>
+      <Navbar />
+      <div className="px-20 pt-8">{children}</div>
     </div>
-  )
+  );
 }
 
-export default MainLayout
+export default MainLayout;
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,4 +1,4 @@
-function Navbar() {
+const Navbar = () => {
   return (
     <div className="navbar bg-zinc-800 text-white">
 
